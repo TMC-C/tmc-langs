ed73554c10b2eb5a993f20bd1dcddc0ccdce3e64
chenhuiz <chenhui.zhao@helsinki.fi>
2020-05-18 17:30:41 +0300

    more tests

--- tmc-langs-csharp/src/test/java/fi/helsinki/cs/tmc/langs/csharp/CSharpPluginTest.java
+++ tmc-langs-csharp/src/test/java/fi/helsinki/cs/tmc/langs/csharp/CSharpPluginTest.java
@@ -3,6 +3,9 @@ package fi.helsinki.cs.tmc.langs.csharp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import fi.helsinki.cs.tmc.langs.abstraction.Strategy;
+import fi.helsinki.cs.tmc.langs.abstraction.ValidationResult;
+import fi.helsinki.cs.tmc.langs.domain.ExerciseDesc;
 import fi.helsinki.cs.tmc.langs.domain.RunResult;
 import fi.helsinki.cs.tmc.langs.domain.TestResult;
 import fi.helsinki.cs.tmc.langs.io.StudentFilePolicy;
@@ -13,6 +16,7 @@ import org.junit.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 
 public class CSharpPluginTest {
 
@@ -42,6 +46,14 @@ public class CSharpPluginTest {
         RunResult runResult = this.csPlugin.runTests(path);
         assertEquals(RunResult.Status.PASSED, runResult.status);
 
+        TestResult testResult = runResult.testResults.get(0);
+        assertTrue(testResult.isSuccessful());
+        assertEquals("PassingSampleTests.ProgramTest.TestGetYear", testResult.getName());
+        assertEquals(2, testResult.points.size());
+        assertTrue(testResult.points.contains("1"));
+        assertTrue(testResult.points.contains("1.2"));
+        assertEquals("", testResult.getMessage());
+        assertEquals(0, testResult.getException().size());
     }
 
     @Test
@@ -49,5 +61,27 @@ public class CSharpPluginTest {
         Path path = TestUtils.getPath(getClass(), "FailingProject");
         RunResult runResult = this.csPlugin.runTests(path);
         assertEquals(RunResult.Status.TESTS_FAILED, runResult.status);
+
+        TestResult testResult = runResult.testResults.get(0);
+        assertTrue(!testResult.isSuccessful());
+        assertTrue(!testResult.getMessage().isEmpty());
+        assertEquals(0, testResult.points.size());
+        assertEquals(3, testResult.getException().size());
+    }
+
+    @Test
+    public void testScanExercise() {
+        Path path = TestUtils.getPath(getClass(), "PassingProject");
+        ExerciseDesc testDesc = this.csPlugin.scanExercise(path, "cs-tests").get();
+        assertEquals("cs-tests", testDesc.name);
+        assertEquals("PassingSampleTests.ProgramTest.TestGetName", testDesc.tests.get(0).name);
+        assertEquals(2, testDesc.tests.get(0).points.size());
+    }
+
+    @Test()
+    public void checkCodeStyleReturnsEmptyResult() {
+        Path path = TestUtils.getPath(getClass(), "PassingProject");
+        ValidationResult result = this.csPlugin.checkCodeStyle(path, new Locale("en"));
+        assertTrue(result.getStrategy() == Strategy.DISABLED);
     }
 }
