8450b325861d0c2173ddd3007711652ba9d81af3
PPeltola <murderbanana@gmail.com>
2020-05-22 14:36:56 +0300

    more debug prints for tests

--- tmc-langs-csharp/src/test/java/fi/helsinki/cs/tmc/langs/csharp/CSharpPluginTest.java
+++ tmc-langs-csharp/src/test/java/fi/helsinki/cs/tmc/langs/csharp/CSharpPluginTest.java
@@ -10,6 +10,7 @@ import fi.helsinki.cs.tmc.langs.domain.RunResult;
 import fi.helsinki.cs.tmc.langs.domain.TestResult;
 import fi.helsinki.cs.tmc.langs.io.StudentFilePolicy;
 import fi.helsinki.cs.tmc.langs.utils.TestUtils;
+import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -49,7 +50,9 @@ public class CSharpPluginTest {
         Path path = TestUtils.getPath(getClass(), "PassingProject");
         System.out.println(path);
         System.out.println(System.getenv("TMC_CSHARP_BOOTSTRAP_PATH"));
+        System.out.println(new File(System.getenv("TMC_CSHARP_BOOTSTRAP_PATH")).exists());
         System.out.println(System.getenv("MSBUILD_EXE_PATH"));
+        System.out.println(new File(System.getenv("MSBUILD_EXE_PATH")).exists());
         RunResult runResult = this.csPlugin.runTests(path);
         assertEquals(RunResult.Status.PASSED, runResult.status);
 
